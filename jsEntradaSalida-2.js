9c807c2b7041c9f849b2a74c8a53c8bf828a6165
brianayala <40811388+brianayala@users.noreply.github.com>
2018-07-03 21:50:41 -0300

    primer guardado
    son los ejercicios del 1 al 4

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,23 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+
+{
+
+//var person = prompt("Please enter your name", "Harry Potter"); 
+	//comentario de una linea
+		/*
+		esto es un comentario en bloque
+		sirve para tomar apuntes
+		*/
+var nombre;
+//nombre=("brian");
+//alert (nombre);
+nombre=prompt("ingrese nombre");
+alert("su nombre es: "+nombre);
+nombre=prompt("ingrese","natalia natalia");
+
+
+
+}
+
---
+++ jsEntradaSalida-5.js
@@ -0,0 +1,11 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function mostrar()
+{	
+	
+
+	var nombre;
+	nombre= prompt ("ingrese nombre")
+	document.getElementById('elNombre').value=nombre;
+}
+
